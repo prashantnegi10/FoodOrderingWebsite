308fd947fdbfff15949d7a2a6310dddf40c70968
prashantnegi10 <54592987+prashantnegi10@users.noreply.github.com>
2023-06-09 11:10:38 +0530

    Add files via upload

---
+++ backend/db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb+srv://prashantnegi478:Prashant10@cluster0.erz4tyn.mongodb.net/fooddeliveryapp?retryWrites=true&w=majority'
+mongoose.set('strictQuery', false);
+module.exports = function (callback) {
+    mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
+        // mongoDbClient.connect(mongoURI, { useNewUrlParser: true }, async(err, result) => {
+        if (err) console.log("---" + err)
+        else {
+            // var database =
+            console.log("connected to mongo")
+            const foodCollection = await mongoose.connection.db.collection("food_items");
+            foodCollection.find({}).toArray(async function (err, data) {
+                const categoryCollection = await mongoose.connection.db.collection("foodCategory");
+                categoryCollection.find({}).toArray(async function (err, Catdata) {
+                    callback(err, data, Catdata);
+
+                })
+            });
+            // listCollections({name: 'food_items'}).toArray(function (err, database) {
+            // });
+            //     module.exports.Collection = database;
+            // });
+        }
+    })
+};
